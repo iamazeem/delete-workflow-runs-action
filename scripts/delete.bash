4f4b2b9fdc3e8a63f3be056074134b04b0a14474
Azeem Sajid <azeem.sajid@gmail.com>
2025-02-24 23:23:23 +0500

    Add list and delete logic

--- scripts/delete.bash
+++ scripts/delete.bash
@@ -16,7 +16,48 @@ trap 'fatal "$LINENO" "$BASH_COMMAND"' ERR
 delete() {
     echo "::group::Deleting workflow runs"
 
-    # TODO: Add deletion logic
+    local GH_RUN_LIST_ARGS=" --limit 100"
+
+    if [[ -n $INCLUDE_DISABLED_WORKFLOWS ]]; then
+        GH_RUN_LIST_ARGS+=" --all"
+    fi
+    if [[ -n $BRANCH ]]; then
+        GH_RUN_LIST_ARGS+=" --branch $BRANCH"
+    fi
+    if [[ -n $COMMIT_SHA ]]; then
+        GH_RUN_LIST_ARGS+=" --commit $COMMIT_SHA"
+    fi
+    if [[ -n $CREATION_DATE ]]; then
+        GH_RUN_LIST_ARGS+=" --created $CREATION_DATE"
+    fi
+    if [[ -n $EVENT ]]; then
+        GH_RUN_LIST_ARGS+=" --event $EVENT"
+    fi
+    if [[ -n $STATUS ]]; then
+        GH_RUN_LIST_ARGS+=" --status $STATUS"
+    fi
+    if [[ -n $USERNAME ]]; then
+        GH_RUN_LIST_ARGS+=" --user $USERNAME"
+    fi
+    if [[ -n $WORKFLOW ]]; then
+        GH_RUN_LIST_ARGS+=" --workflow $WORKFLOW"
+    fi
+
+    if ((KEEP > 0)); then
+        ((KEEP += 1))
+    fi
+
+    while :; do
+        # shellcheck disable=SC2086
+        RUN_IDS=$(gh run list $GH_RUN_LIST_ARGS --json databaseId --jq '.[] | join("")' | tail -n +"$KEEP")
+        if [[ -z $RUN_IDS ]]; then
+            break
+        fi
+
+        for RUN_ID in $RUN_IDS; do
+            gh run delete "$RUN_ID"
+        done
+    done
 
     echo "::endgroup::"
 }
